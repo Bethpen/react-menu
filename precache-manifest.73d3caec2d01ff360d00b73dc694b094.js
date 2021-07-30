5aa5988e20278c3230bce85ef4c5f017084bc0eb
Bethpen <64626802+Bethpen@users.noreply.github.com>
2021-07-30 16:33:12 +0100

    Updates

---
+++ precache-manifest.73d3caec2d01ff360d00b73dc694b094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9038514ab14b5b6a538878b0fcccfa4",
+    "url": "/react-menu/index.html"
+  },
+  {
+    "revision": "abcf0b99b72309551607",
+    "url": "/react-menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/react-menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abcf0b99b72309551607",
+    "url": "/react-menu/static/js/main.dd966c24.chunk.js"
+  },
+  {
+    "revision": "16969508fe17c64f3354",
+    "url": "/react-menu/static/js/runtime-main.a64c297e.js"
+  }
+]);
